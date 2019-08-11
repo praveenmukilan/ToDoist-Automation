1a3ee0af87dbe2068ce65bd5155639bc53f2f151
praveen ms <praveenmukilan@gmail.com>
2019-08-12 01:38:29 +0800

    Command is abstracted from Commands

---
+++ src/com/slate/api/Command.java
@@ -0,0 +1,44 @@
+package com.slate.api;
+
+import java.util.UUID;
+import org.json.JSONObject;
+
+public class Command {
+		
+		Types type;
+		String temp_id;
+		String uuid;
+		JSONObject args;
+		JSONObject cmdJson;
+		
+		Command(Types type, JSONObject args){
+			this.type = type;
+			this.temp_id = this.getRandomUuid();
+			this.uuid = this.getRandomUuid();
+			this.args = args;
+			this.setCmdJson();
+		}
+		
+		public void setCmdJson() {
+			this.cmdJson = new JSONObject()
+					.put("type", type)
+					.put("temp_id", this.temp_id)
+					.put("uuid", this.uuid)
+					.put("args", args);
+		}
+		
+		public JSONObject getCmdJson() {
+			return this.cmdJson;
+		}
+		
+		public String getRandomUuid() {
+			return UUID.randomUUID().toString();
+		}
+		
+		enum Types {
+			project_add
+		}
+		
+
+	
+}
